e1aa9b777a3566c8da88725a24454ad2cfd1031d
Germain AUBERT <germain.aubert@gmail.com>
2021-09-19 12:46:14 +0200

    template init

---
+++ html/src/net/mgsx/dl18/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.mgsx.dl18.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.mgsx.dl18.DL18;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new DL18();
+        }
+}
